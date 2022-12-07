ef3bb5650b68804ceed42edd4f9cccc901d3b529
sixtos04@gmail.com <sixtos04@gmail.com>
2022-12-07 10:15:24 -0600

    Reset project

---
+++ PokeAPI/PokeAPI.cs
@@ -0,0 +1,88 @@
+using System.Net.Http.Json;
+public class PokeAPI2
+{
+    public HttpClient client;
+    public int POKEMON_COUNT = 1154;
+    public NamedAPIResourceList pokemonList;
+    public Dictionary<int, string> diccPokeNames, diccPokeURLs;
+    public Dictionary<int, Pokemon> diccPokemons;
+	public PokeAPI2()
+	{
+        client = new HttpClient();
+        client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(
+                "application/json")
+            );
+        diccPokeNames = new Dictionary<int, string>();
+        diccPokeURLs = new Dictionary<int, string>();
+	}
+
+    public class NamedAPIResource
+    {
+        public string name {get; set;}
+        public string url {get; set;}
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    public class NamedAPIResourceList
+    {
+        public int count {get; set;}
+        public string next {get; set;}
+        public string previous {get; set;}
+        public NamedAPIResource[] results {get; set;}
+    }
+
+    public async Task<NamedAPIResourceList> GetPokemonList()
+    {
+        HttpResponseMessage resp = await client.GetAsync("pokemon/?limit=10000");
+        if(resp.IsSuccessStatusCode)
+        {
+            pokemonList = await resp.Content.ReadFromJsonAsync<NamedAPIResourceList>();
+            POKEMON_COUNT = pokemonList.count;
+            for (int i = 0; i < POKEMON_COUNT; i++)
+            {
+                if(!diccPokeNames.ContainsKey(i))
+                    diccPokeNames.Add(i, pokemonList.results[i].name);
+                if(!diccPokeURLs.ContainsKey(i))
+                    diccPokeURLs.Add(i, pokemonList.results[i].url);
+            }
+            return pokemonList;
+        }
+        return null;
+    }
+
+    public async Task<Pokemon> GetPokemon(int id)
+    {
+        try
+		{
+            if (diccPokemons.ContainsKey(id))
+                return diccPokemons[id];
+
+			if (1 <= id && id <= POKEMON_COUNT)
+			{
+				Pokemon poke;
+				HttpResponseMessage response = await client.GetAsync(new Uri(diccPokeURLs[id]));
+				if (response.IsSuccessStatusCode)
+				{
+					poke = await response.Content.ReadFromJsonAsync<Pokemon>();
+                    diccPokemons.Add(id, poke);
+					return poke;
+				}
+			}
+			
+			return null;
+		}
+		catch (Exception e)
+		{
+			await global::System.Console.Out.WriteLineAsync(e.ToString());
+			throw;
+		}
+    }
+
+}
---
+++ PokeAPI/Pokemon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+public class Pokemon
+{
+	public int id {get; set;}
+	public string name {get; set;}
+	public int base_experience {get; set;}
+	public int height {get; set;} /* in decimeters */
+	public int weight {get; set;} /* in hectograms */
+	public Ability[] abilities {get; set;}
+	public NamedAPIResource[] forms {get; set;}
+	
+	public PokemonMove[] moves {get; set;}
+	public PokemonSprite sprites {get; set;}
+	public NamedAPIResource species {get; set;}
+	
+	public PokemonStat[] stats {get; set;}
+	public PokemonType[] types {get; set;}
+
+	public class Ability
+	{
+		public bool is_hidden {get; set;}
+		public int slot {get; set;}
+		public NamedAPIResource ability {get; set;}
+		
+	}
+	public class PokemonMove
+	{
+		public NamedAPIResource move {get; set;}
+		
+	}
+	public class PokemonSprite
+	{
+		public string front_default {get; set;}
+		public string front_shiny {get; set;}
+		public string front_female {get; set;}
+		public string front_shiny_female {get; set;}
+		public string back_default {get; set;}
+		public string back_shiny {get; set;}
+		public string back_female {get; set;}
+		public string back_shiny_female {get; set;}
+	}
+	public class PokemonStat
+	{
+		public NamedAPIResource stat {get; set;}
+		
+		public int effort {get; set;}
+		public int base_stat {get; set;}
+	}
+	public class PokemonType
+	{
+		public int slot {get; set;}
+		public NamedAPIResource type {get; set;}
+		
+	}
+}
